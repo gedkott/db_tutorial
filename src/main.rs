cb0356185e93847070421946bb224745e015245e
Gedalia Kott <gedkott@gmail.com>
2021-09-13 17:43:57 -0700

    implement a WIP repl struct that will hold the b tree storage reference and the input buffer

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,61 @@
 use std::io::Write;
 use std::io::{stdin, stdout};
 
+struct Repl {
+    input_buffer: String,
+    b_tree: Vec<u8>,
+}
+
+impl Repl {
+    fn prepare_statement(&self) -> Result<Statement, StatementError> {
+        let clean_input = self.input_buffer.trim();
+        println!("processing statement {:?}", clean_input);
+        if clean_input.starts_with("insert") {
+            let mut parts = clean_input.split(' ');
+            match (parts.nth(1), parts.nth(2), parts.nth(3)) {
+                (Some(id), Some(username), Some(email)) => Ok(Statement::Insert {
+                    data: (id, username, email),
+                }),
+                _ => Err(StatementError::Sql),
+            }
+        } else if clean_input.starts_with("select") {
+            Ok(Statement::Select)
+        } else {
+            Err(StatementError::Sql)
+        }
+    }
+    fn execute_statement(&self, statement: Statement) -> Result<Results, StatementError> {
+        match statement {
+            Statement::Insert {
+                data: (id, name, email),
+            } => {
+                println!("executing insert statement");
+                Ok(Results {})
+            }
+            Statement::Select => {
+                println!("executing select statement");
+                Ok(Results {})
+            }
+        }
+    }
+    fn read_user_input(&mut self) -> Result<(), ReplErr> {
+        print!("db > ");
+        self.input_buffer.clear();
+        flush_stdout()
+            .and_then(|_| stdin().read_line(&mut self.input_buffer))
+            .and_then(move |n| ensure_stdout_newline((n, &mut self.input_buffer)))
+            .map_err(ReplErr::IOErr)
+    }
+}
+
 enum MetaCommand {
     Exit,
     Unsupported,
 }
 
-enum StatementType {
-    Insert,
+enum Statement<'a> {
     Select,
-}
-
-struct Statement {
-    statement_type: StatementType,
+    Insert { data: (&'a str, &'a str, &'a str) },
 }
 
 #[derive(Debug)]
@@ -23,9 +66,9 @@ enum StatementError {
 #[derive(Debug)]
 struct Results {}
 
-enum ReplAction<'a> {
+enum ReplAction {
     Exit,
-    Statement { original_input: &'a str },
+    Statement,
     Unsupported { message: String },
 }
 
@@ -36,17 +79,24 @@ enum ReplErr {
 
 fn main() {
     // initialize any thing we need for the REPL
-    let mut input_buffer = String::new();
+    let input_buffer = String::new();
+
+    // create Repl
+    let mut repl = Repl {
+        input_buffer,
+        b_tree: vec![],
+    };
 
     // Loop until "exit" input is provided
     loop {
-        input_buffer.clear();
-        print!("db > ");
-        match read_user_input(&mut input_buffer) {
-            Ok(input) => match input.into() {
+        match repl.read_user_input() {
+            Ok(()) => match &repl.input_buffer[..].into() {
                 ReplAction::Exit => break,
-                ReplAction::Statement { original_input } => {
-                    match prepare_statement(original_input).and_then(execute_statement) {
+                ReplAction::Statement => {
+                    match repl
+                        .prepare_statement()
+                        .and_then(|s| repl.execute_statement(s))
+                    {
                         Ok(results) => {
                             println!("results from statement have arrived {:?}", results)
                         }
@@ -62,47 +112,15 @@ fn main() {
     }
 }
 
-fn execute_statement(statement: Statement) -> Result<Results, StatementError> {
-    match statement.statement_type {
-        StatementType::Insert => {
-            println!("executing insert statement");
-            Ok(Results {})
-        }
-        StatementType::Select => {
-            println!("executing select statement");
-            Ok(Results {})
-        }
-    }
-}
-
-fn prepare_statement(original_input: &str) -> Result<Statement, StatementError> {
-    println!("processing statement {:?}", original_input);
-    let statement_type = if original_input.starts_with("insert") {
-        Ok(StatementType::Insert)
-    } else if original_input.starts_with("select") {
-        Ok(StatementType::Select)
-    } else {
-        Err(StatementError::Sql)
-    };
-    statement_type.map(|st| Statement { statement_type: st })
-}
-
-fn read_user_input(input_buffer: &mut String) -> Result<&str, ReplErr> {
-    flush_stdout()
-        .and_then(|_| stdin().read_line(input_buffer))
-        .and_then(move |n| ensure_stdout_newline((n, input_buffer)))
-        .map_err(ReplErr::IOErr)
-}
-
 fn flush_stdout() -> Result<(), std::io::Error> {
     stdout().flush()
 }
 
-fn ensure_stdout_newline((n, input): (usize, &mut String)) -> Result<&str, std::io::Error> {
+fn ensure_stdout_newline((n, input): (usize, &mut String)) -> Result<(), std::io::Error> {
     match (n, &input[..]) {
         (0, "") => {
             // Standard EOF scenario; no newline character for sure
-            stdout().write_all(b"\n").map(move |_| &input[0..n])
+            stdout().write_all(b"\n")
         }
         _ => {
             // when reading EOF at end of *line*, read_line reads EOF twice
@@ -110,28 +128,25 @@ fn ensure_stdout_newline((n, input): (usize, &mut String)) -> Result<&str, std::
             // so we print a new line to make sure that we don't print anything
             // on the same line as the original prompt; def no newline
             if !input.contains('\n') {
-                stdout().write_all(b"\n").map(move |_| &input[0..n])
+                stdout().write_all(b"\n")
             } else {
-                // input contains a newline (which is prob already handled by stdout)
-                // and its not needed for the caller in our application since the newline
-                // character is meaningless in the user input buffer
-                Ok(&input[0..n - 1])
+                Ok(())
             }
         }
     }
 }
 
-impl<'a> From<&'a str> for ReplAction<'a> {
+impl<'a> From<&'a str> for ReplAction {
     fn from(s: &'a str) -> Self {
         if let Some('.') = s.chars().next() {
             match s.into() {
                 MetaCommand::Exit => ReplAction::Exit,
                 MetaCommand::Unsupported => ReplAction::Unsupported {
-                    message: format!("command {:?} is unsupported", s),
+                    message: format!("meta command {:?} is unsupported", s),
                 },
             }
         } else {
-            ReplAction::Statement { original_input: s }
+            ReplAction::Statement
         }
     }
 }

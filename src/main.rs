ccd8f151c69475d34a38ee0e95e4a81983867aa0
Gedalia Kott <gedkott@gmail.com>
2021-09-14 21:53:49 -0700

    complete part 3 from the database tutorial with many thoughts to consider

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,66 @@
+use std::collections::HashMap;
 use std::convert::TryInto;
-use std::io::Write;
 use std::io::{stdin, stdout};
+use std::io::{Read, Write};
+use std::str::from_utf8;
 
 enum MetaCommand {
     Exit,
     Unsupported,
 }
 
-enum Statement<'a> {
-    Insert { row: Row<'a> },
+enum Statement {
+    Insert { row: Row },
     Select,
 }
 
-struct Row<'a> {
+#[derive(Debug)]
+struct Row {
     id: u32,
-    username: &'a [u8; 32],
-    email: &'a [u8; 255],
+    username: [u8; 32],
+    email: [u8; 255],
+}
+
+fn serialize_row(row: &Row) -> [u8; 291] {
+    let mut buf = [0u8; 291];
+
+    let ibytes = &u32::to_be_bytes(row.id)[..];
+    let ubytes = &row.username[..];
+    let ebytes = &row.email[..];
+
+    (&mut buf[..4]).write_all(ibytes).unwrap();
+    (&mut buf[4..36]).write_all(ubytes).unwrap();
+    (&mut buf[36..]).write_all(ebytes).unwrap();
+
+    buf
+}
+
+fn deserialize_row(buf: &[u8; 291]) -> Row {
+    let mut ibytes = [0u8; 4];
+    let mut username = [0u8; 32];
+    let mut email = [0u8; 255];
+
+    (&buf[..4]).read_exact(&mut ibytes).unwrap();
+    (&buf[4..36]).read_exact(&mut username).unwrap();
+    (&buf[36..]).read_exact(&mut email).unwrap();
+
+    let id = u32::from_be_bytes(ibytes);
+
+    Row {
+        id,
+        username,
+        email,
+    }
+}
+
+#[derive(Debug)]
+struct Page {
+    buffer: [u8; 4096],
+}
+
+struct Table {
+    num_rows: u32,
+    pages: HashMap<u32, Page>,
 }
 
 #[derive(Debug)]
@@ -24,7 +69,10 @@ enum StatementError {
 }
 
 #[derive(Debug)]
-struct Results {}
+enum ReplResult {
+    Rows(Vec<Row>),
+    Success,
+}
 
 enum ReplAction<'a> {
     Exit,
@@ -41,6 +89,11 @@ fn main() {
     // initialize any thing we need for the REPL
     let mut input_buffer = String::new();
 
+    let mut table = Table {
+        pages: HashMap::new(),
+        num_rows: 0,
+    };
+
     // Loop until "exit" input is provided
     loop {
         input_buffer.clear();
@@ -49,10 +102,26 @@ fn main() {
             Ok(input) => match input.into() {
                 ReplAction::Exit => break,
                 ReplAction::Statement { original_input } => {
-                    match prepare_statement(original_input).and_then(execute_statement) {
-                        Ok(results) => {
-                            println!("results from statement have arrived {:?}", results)
-                        }
+                    match prepare_statement(original_input)
+                        .and_then(|s| execute_statement(s, &mut table))
+                    {
+                        Ok(results) => match results {
+                            ReplResult::Rows(rows) => {
+                                rows.iter().for_each(|r| {
+                                    println!(
+                                        "{:?}, {:?}, {:?}",
+                                        r.id,
+                                        from_utf8(&r.username[..])
+                                            .unwrap()
+                                            .trim_matches(char::from(0)),
+                                        from_utf8(&r.email[..])
+                                            .unwrap()
+                                            .trim_matches(char::from(0))
+                                    );
+                                });
+                            }
+                            _ => println!("results from statement have arrived {:?}", results),
+                        },
                         Err(e) => println!("db message: {:?}", &e),
                     }
                 }
@@ -65,22 +134,44 @@ fn main() {
     }
 }
 
-fn execute_statement(statement: Statement) -> Result<Results, StatementError> {
+fn execute_statement(
+    statement: Statement,
+    table: &mut Table,
+) -> Result<ReplResult, StatementError> {
     match statement {
-        Statement::Insert {
-            row:
-                Row {
-                    id,
-                    username,
-                    email,
-                },
-        } => {
+        Statement::Insert { row } => {
             println!("executing insert statement");
-            Ok(Results {})
+            let bytes = serialize_row(&row);
+            let rows_per_page = 4096 / 291;
+            let page_num = table.num_rows / rows_per_page;
+            let page = table.pages.entry(page_num).or_insert_with(|| Page {
+                buffer: [0u8; 4096],
+            });
+            let row_offset = table.num_rows % rows_per_page;
+            let byte_offset = row_offset * 291;
+            let mut point =
+                &mut page.buffer[byte_offset as usize..(byte_offset as usize + 291 + 1)];
+            point.write_all(&bytes).unwrap();
+            println!("{:?}", point);
+            table.num_rows += 1;
+            Ok(ReplResult::Success)
         }
         Statement::Select => {
             println!("executing select statement");
-            Ok(Results {})
+            let rows_per_page = 4096 / 291;
+            let mut rows = Vec::new();
+
+            for i in 0..table.num_rows {
+                let page_num = i / rows_per_page;
+                let page = &table.pages[&page_num];
+                let row_offset = i % rows_per_page;
+                let byte_offset = row_offset * 291;
+                let point = &page.buffer[byte_offset as usize..byte_offset as usize + 291];
+                let sized_point = point.try_into().unwrap();
+                let row = deserialize_row(sized_point);
+                rows.push(row);
+            }
+            Ok(ReplResult::Rows(rows))
         }
     }
 }
@@ -89,20 +180,36 @@ fn prepare_statement(original_input: &str) -> Result<Statement, StatementError> 
     println!("processing statement {:?}", original_input);
     if original_input.starts_with("insert") {
         let mut parts = original_input.split(' ');
-        match (
-            parts.nth(1),
-            parts.nth(2),
-            parts.nth(3),
-        ) {
+        let id = parts.nth(1);
+        let username = parts.next();
+        let email = parts.next();
+        match (id, username, email) {
             (Some(id), Some(username), Some(email)) => {
-                let id = id.parse().ok().unwrap();
-                let username = username.as_bytes()[..32].try_into().unwrap();
-                let email = email.as_bytes()[..32].try_into().unwrap();
+                let id = id.parse().map_err(|_| StatementError::Sql)?;
+
+                let mut un = [0u8; 32];
+                for (i, b) in username.as_bytes().iter().enumerate() {
+                    if i == 32 {
+                        break;
+                    } else {
+                        un[i] = *b;
+                    }
+                }
+
+                let mut em = [0u8; 255];
+                for (i, b) in email.as_bytes().iter().enumerate() {
+                    if i == 255 {
+                        break;
+                    } else {
+                        em[i] = *b;
+                    }
+                }
+
                 Ok(Statement::Insert {
                     row: Row {
                         id,
-                        username,
-                        email,
+                        username: un,
+                        email: em,
                     },
                 })
             }
@@ -119,6 +226,7 @@ fn read_user_input(input_buffer: &mut String) -> Result<&str, ReplErr> {
     flush_stdout()
         .and_then(|_| stdin().read_line(input_buffer))
         .and_then(move |n| ensure_stdout_newline((n, input_buffer)))
+        .map(|s| s.trim())
         .map_err(ReplErr::IOErr)
 }
 

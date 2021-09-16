f9f5f5596838520d63a8573785e7ef6e69af05bd
Gedalia Kott <gedkott@gmail.com>
2021-09-15 21:19:33 -0700

    free the tests of the crazy byte processing (see comments)

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,8 +1,8 @@
-use std::io::{Read, Write};
+use std::io::Write;
 use std::iter::repeat;
 use std::process::{Command, Stdio};
 
-fn run_script<T: AsRef<str>>(commands: &[T]) -> Vec<String> {
+fn run_script(commands: Vec<String>) -> Vec<String> {
     let mut child = Command::new("cargo")
         .arg("run")
         .stdin(Stdio::piped())
@@ -10,89 +10,59 @@ fn run_script<T: AsRef<str>>(commands: &[T]) -> Vec<String> {
         .spawn()
         .expect("Failed to spawn child process");
 
-    let mut stdin = child.stdin.take().expect("Failed to open stdin");
-    let stdout = &mut child.stdout.take().expect("Failed to open stdout");
-    let cmds = commands.iter();
-    let mut outputs = vec![];
-    cmds.for_each(|cmd| {
-        stdin
-            .write_all(&[cmd.as_ref().as_bytes(), b"\n"].concat())
-            .expect("Failed to write to stdin");
+    let mut stdin = child.stdin.take().expect("failed to get stdin");
 
-        let end_of_output = b"\ndb > ";
-        let mut seen = vec![];
-        let mut output: Vec<u8> = stdout
-            .bytes()
-            .filter_map(|b| b.ok())
-            .take_while(|b| {
-                if seen.len() < end_of_output.len() {
-                    seen.push(*b)
-                } else {
-                    seen.remove(0);
-                    seen.push(*b);
-                }
-
-                &seen != end_of_output
-            })
-            .collect();
-
-        match seen.last() {
-            Some(c) => output.push(*c),
-            _ => (),
-        }
-
-        let buf = String::from_utf8_lossy(&output).to_string();
-        if buf != "" {
-            outputs.push(buf);
-        }
+    // If the child process fills its stdout buffer, it may end up
+    // waiting until the parent reads the stdout, and not be able to
+    // read stdin in the meantime, causing a deadlock.
+    // Writing from another thread ensures that stdout is being read
+    // at the same time, avoiding the problem.
+    let handle = std::thread::spawn(move || {
+        commands.iter().for_each(|cmd| {
+            stdin
+                .write_all(&[cmd.as_bytes(), b"\n"].concat())
+                .expect("Failed to write to stdin");
+        });
     });
 
-    let _ = child.wait().expect("Failed to read stdout");
-    outputs
+    // wait for output also attempts to read from the buffer for stdout which stops us from hanging
+    let output = child.wait_with_output().expect("Failed to read stdout");
+    handle.join().unwrap();
+    let stringified = String::from_utf8_lossy(&output.stdout).to_string();
+    stringified.split("\n").map(String::from).collect()
 }
 
 #[test]
 fn database_inserts_and_retrieves_a_row() {
-    let output = run_script(&["insert 1 user1 person1@example.com", "select", ".exit"]);
+    let output = run_script(vec![
+        "insert 1 user1 person1@example.com".into(),
+        "select".into(),
+        ".exit".into(),
+    ]);
     assert_eq!(
         output,
         vec![
-            vec![
-                "db > processing statement \"insert 1 user1 person1@example.com\"",
-                "executing insert statement",
-                "result Success",
-                "db > ",
-            ]
-            .join("\n"),
-            vec![
-                "processing statement \"select\"",
-                "executing select statement",
-                "1, \"user1\", \"person1@example.com\"",
-                "db > "
-            ]
-            .join("\n")
+            "db > processing statement \"insert 1 user1 person1@example.com\"",
+            "executing insert statement",
+            "result Success",
+            "db > processing statement \"select\"",
+            "executing select statement",
+            "1, \"user1\", \"person1@example.com\"",
+            "db > "
         ]
     );
 }
 
 #[test]
 fn prints_error_message_when_table_is_full() {
-    let insert_cmds: Vec<String> = (1..1402)
+    let mut cmds: Vec<String> = (1..1402)
         .map(|i| format!("insert {} user{} person{}@example.com", i, i, i))
         .collect();
+    cmds.push(".exit".into());
 
-    let cmds = [insert_cmds, vec![".exit".into()]].concat();
-    let output = run_script(&cmds);
-    assert_eq!(
-        *output.last().unwrap(),
-        vec![
-            "processing statement \"insert 1401 user1401 person1401@example.com\"",
-            "executing insert statement",
-            "db message: Execute(TableFull)",
-            "db > "
-        ]
-        .join("\n")
-    );
+    let output = run_script(cmds);
+    let relevant_output = output.get(output.len() - 2).unwrap();
+    assert_eq!(relevant_output, "db message: Execute(TableFull)",);
 }
 
 #[test]
@@ -100,27 +70,22 @@ fn allows_inserting_and_selecting_strings_that_are_the_max_length() {
     let long_username: String = repeat("a").take(32).collect();
     let long_email: String = repeat("a").take(255).collect();
 
-    let cmds = [
+    let cmds = vec![
         format!("insert 1 {} {}", long_username, long_email),
         "select".into(),
         ".exit".into(),
     ];
-    let output = run_script(&cmds);
+    let output = run_script(cmds);
     assert_eq!(
         output,
         vec![
-            vec![
-                "db > processing statement \"insert 1 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"", 
-                "executing insert statement",
-                "result Success",
-                "db > "
-            ].join("\n"), 
-            vec![
-                "processing statement \"select\"",
-                "executing select statement",
-                "1, \"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\", \"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"",
-                "db > "
-            ].join("\n")
+            "db > processing statement \"insert 1 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"", 
+            "executing insert statement",
+            "result Success",
+            "db > processing statement \"select\"",
+            "executing select statement",
+            "1, \"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\", \"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"",
+            "db > "
         ]
     );
 }
@@ -130,20 +95,18 @@ fn prints_error_messages_if_strings_are_too_long() {
     let long_username: String = repeat("a").take(33).collect();
     let long_email: String = repeat("a").take(256).collect();
 
-    let cmds = [
+    let cmds = vec![
         format!("insert 1 {} {}", long_username, long_email),
         ".exit".into(),
     ];
-    let output = run_script(&cmds);
+    let output = run_script(cmds);
     assert_eq!(
         output,
-        vec![
             vec![
                 "db > processing statement \"insert 1 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"",
                 "db message: Statement(TooLong)",
                 "db > "
-            ].join("\n")
-        ]
+            ]
     );
 }
 
@@ -152,18 +115,46 @@ fn prints_error_messages_if_id_is_negative() {
     let long_username = "a";
     let long_email = "a";
 
-    let cmds = [
+    let cmds = vec![
         format!("insert -1 {} {}", long_username, long_email),
         ".exit".into(),
     ];
-    let output = run_script(&cmds);
+    let output = run_script(cmds);
     assert_eq!(
         output,
-        vec![vec![
+        vec![
             "db > processing statement \"insert -1 a a\"",
             "db message: Statement(InvalidId)",
             "db > "
         ]
-        .join("\n")]
+    );
+}
+
+#[test]
+fn keeps_data_after_closing_connection() {
+    let output1 = run_script(vec![
+        "insert 1 user1 person1@example.com".into(),
+        ".exit".into(),
+    ]);
+    assert_eq!(
+        output1,
+        vec![vec![
+            "db > processing statement \"insert 1 user1 person1@example.com\"",
+            "executing insert statement",
+            "result Success",
+            "db > ",
+        ]
+        .join("\n"),]
+    );
+
+    let output2 = run_script(vec!["select".into(), ".exit".into()]);
+    assert_eq!(
+        output2,
+        vec![
+            "processing statement \"select\"",
+            "executing select statement",
+            "1, \"user1\", \"person1@example.com\"",
+            "db > "
+        ]
     );
 }
